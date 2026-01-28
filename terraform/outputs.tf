0c305cb0c318128097c317317141e25eedd71dd5
Ayobami Agboola <iamuskar26@gmail.com>
2026-01-28 15:07:54 +0000

    feat: add static_ip output with description for web server's public IP

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "public_ip" {
   value = aws_eip.static_ip.public_ip
 }
+
+output "static_ip" {
+  value       = aws_eip.static_ip.public_ip
+  description = "The public IP of the web server"
+}
