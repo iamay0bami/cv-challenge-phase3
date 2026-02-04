e707483f1111a0a21fb98b705fc4c9c29560d24e
Ayobami Agboola <iamuskar26@gmail.com>
2026-02-04 15:06:46 +0000

    feat: add default tags for environment and service to AWS provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,13 @@ resource "time_sleep" "wait_30_seconds" {
 
 provider "aws" {
   region = var.aws_region
+
+  default_tags {
+    tags = {
+      Environment = "Dev"
+      Service     = "CV-Challenge"
+    }
+  }
 }
 
 resource "aws_security_group" "web_sg" {
